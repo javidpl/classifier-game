b09f79e748bb5534df0e5dd4e838eee5a8e8cce0
javidpl <48187792+javidpl@users.noreply.github.com>
2021-09-26 09:19:00 +0200

    26/09/2021/1

--- games/animals.js
+++ games/animals.js
@@ -2,45 +2,57 @@
 const animals = {
     groups: {
         mammals: {
-            color: 'rgb(255, 205, 172)',
+            title : 'Säugetiere',
+            background: 'rgb(255, 205, 172)',
             items: {
                 dolphin: {
+                    title: "Delphin",
                     imgUrl: 'https://live.staticflickr.com/4061/4589042409_fae70bb32f_b.jpg'
                 },
                 bear: {
+                    title: "Bär",
                     imgUrl: 'https://memegenerator.net/img/images/300x300/13692847.jpg'
                 },
                 mouse: {
+                    title: "Maus",
                     imgUrl: 'https://squeaksandnibbles.com/wp-content/uploads/2018/06/how-long-do-mice-live.jpg'
                 }
             }
 
         },
         reptiles: {
-            color: 'lightgreen',
+            title : 'Reptilien',
+            background: 'lightgreen',
             items: {
                 chameleon: {
+                    title: "Chamäleon",
                     imgUrl: 'https://live.staticflickr.com/65535/48329213772_3819880e7f_z.jpg'
                 },
                 turtle: {
+                    title: "Schildkröte",
                     imgUrl: 'https://cdn.rewild.org/2020/06/IMG_1109-1024x725.jpg'
                 },
                 crocodile: {
+                    title: "Krokodil",
                     imgUrl: 'https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvYXNzZXRzLzMzOGVjMmMzMWQwNjFjMDU5NF84MDBweC1TYWx0d2F0ZXJfY3JvY29kaWxlLmpwZyJdLFsicCIsImNvbnZlcnQiLCItYXV0by1vcmllbnQgIl0sWyJwIiwidGh1bWIiLCI4MDB4NTMzKzArMzgiXSxbInAiLCJjb252ZXJ0IiwiLXF1YWxpdHkgODEgLWF1dG8tb3JpZW50Il0sWyJwIiwidGh1bWIiLCI3ODB4NTIwIyJdXQ'
                 }
             }
 
         },
         amphibians: {
-            color: 'lightblue',
+            title: 'Amphibien',
+            background: 'lightblue',
             items: {
                 salamander: {
+                    title: "Salamander",
                     imgUrl: 'https://image.geo.de/30046012/t/Av/v3/w1440/r1/-/salamander-gross-jpg--19514-.jpg'
                 },
                 frog: {
+                    title: "Frosch",
                     imgUrl: 'https://dr282zn36sxxg.cloudfront.net/datastreams/f-d%3A2f313cefad5bc1901b975a8c9089a645ed96708ebcf4a07f94872409%2BIMAGE_TINY%2BIMAGE_TINY.1'
                 },
                 axolotl: {
+                    title: "Axolotl",
                     imgUrl: 'https://images.immediate.co.uk/production/volatile/sites/4/2020/03/GettyImages-1058304880-c-0b54061-scaled.jpg?quality=90&resize=768%2C574'
                 }
             }
@@ -49,4 +61,5 @@ const animals = {
     }
 
 }
+
 
---
+++ games/planets.js
@@ -0,0 +1,57 @@
+
+
+const planets = {
+    groups: {
+        terrestrial: {
+            items: {
+                Mercury: {
+                    title: 'Merkur',
+                    imgUrl: 'https://scx1.b-cdn.net/csz/news/800a/2015/whatsimporta.jpg'
+                },
+                Venus: {
+                    title: 'Venus',
+                    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped%29.jpg'
+                },
+                Earth: {
+                    title: 'Erde',
+                    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/1200px-The_Earth_seen_from_Apollo_17.jpg'
+                },
+                Mars: {
+                    title: 'Mars',
+                    imgUrl: 'https://spacegeneration.org/wp-content/uploads/2021/05/mars.jpeg'
+                }
+            }
+        },
+        jovian: {
+            items: {
+                Jupiter: {
+                    title: 'Jupiter',
+                    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg'
+                },
+                Saturn: {
+                    title: 'Saturn',
+                    imgUrl: 'https://scx1.b-cdn.net/csz/news/800a/2015/theplanetsat.jpg'
+                },
+                Uranus: {
+                    title: 'Uranus',
+                    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Uranus2.jpg/1200px-Uranus2.jpg'
+                },
+                Neptune: {
+                    title: 'Neptun',
+                    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg'
+                }
+            }
+        }
+    }
+}
+
+/*
+https://scx1.b-cdn.net/csz/news/800a/2015/whatsimporta.jpg
+https://upload.wikimedia.org/wikipedia/commons/c/c7/PIA23791-Venus-RealAndEnhancedContrastViews-20200608_%28cropped%29.jpg
+https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/1200px-The_Earth_seen_from_Apollo_17.jpg
+https://spacegeneration.org/wp-content/uploads/2021/05/mars.jpeg
+https://upload.wikimedia.org/wikipedia/commons/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg
+https://scx1.b-cdn.net/csz/news/800a/2015/theplanetsat.jpg
+https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Uranus2.jpg/1200px-Uranus2.jpg
+https://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg
+*/
